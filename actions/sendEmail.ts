2b247f45c0a6d407bcd03387a19b54cc8f4cd52d
Jakob Lamber <mr.jamlamb@gmail.com>
2023-12-30 18:29:41 -0600

    Changed emailing logic to pass html element

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -4,6 +4,7 @@ import { getErrorMessage, validateString } from "@/lib/utils";
 import { Resend } from "resend";
 import ContactFormEmail from "@/email/contact-form-email";
 import React from "react";
+import { renderAsync } from "@react-email/components";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -25,15 +26,20 @@ export const sendEmail = async (formData: FormData) => {
   }
   let data;
   try {
+    // Render your React component to an HTML string
+    const html = await renderAsync(
+      React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      })
+    );
+
     await resend.emails.send({
-      from: "Contact Form<onboarding@resend.dev>",
+      from: "Contact Form <onboarding@resend.dev>",
       to: "mr.jamlamb@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
-      react: React.createElement(ContactFormEmail, {
-        message: message as string,
-        senderEmail: senderEmail as string,
-      }),
+      html: html, // Use the rendered HTML here
     });
   } catch (error: unknown) {
     return {
